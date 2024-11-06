2c99a8eac299693df83c8637d22e2a7e5a2f5559
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-11-06 06:51:44 -0800

    Implement Frontend changes��

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -35,7 +35,7 @@ const LeftSidebar = () => {
         })}
       </div>
 
-      {/* <SignedOut> */}
+      <SignedOut>
         <div className='flex flex-col gap-3'>
           <Link href="/sign-in">
             <Button className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
@@ -63,7 +63,7 @@ const LeftSidebar = () => {
             </Button>
           </Link>
         </div>
-      {/* </SignedOut> */}
+      </SignedOut>
     </section>
   )
 }
