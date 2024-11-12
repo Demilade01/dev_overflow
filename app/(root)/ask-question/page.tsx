388aa0ff60b148a6be15a6127810f161b6b823fd
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-11-12 07:28:21 -0800

    implement community page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -4,13 +4,13 @@ import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = async () => {
+const Page = async () => {
   const { userId } = await auth();
 
   if(!userId) redirect('/sign-in');
 
   const mongoUser = await getUserById({ userId });
-  
+
   return (
     <div>
       <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
@@ -21,4 +21,4 @@ const page = async () => {
   )
 }
 
-export default page
+export default Page
