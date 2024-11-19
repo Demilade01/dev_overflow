808ca63f6ad88ae82809716f2b0ce7e591954ddc
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-11-19 01:51:29 -0800

    implement collections page && add interactions to the database

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,28 +1,25 @@
 import QuestionCard from '@/components/cards/QuestionCard'
-import HomeFilter from '@/components/home/HomeFilter'
 import Filter from '@/components/shared/Filter'
 import NoResult from '@/components/shared/NoResult'
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
-import { Button } from '@/components/ui/button'
-import { HomePageFilters } from '@/constants/filter'
-import { getQuestions } from '@/lib/actions/question.action'
-import Link from 'next/link'
+import { QuestionFilters } from '@/constants/filter'
+import { getSavedQuestions } from '@/lib/actions/user.action'
+import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 
-const Home = async () => {
-  const result = await getQuestions({});
+const Collection = async () => {
+  const { userId } = await auth();
+
+  if(!userId) return null;
+
+  const result = await getSavedQuestions({
+    clerkId: userId,
+  });
 
   return (
     <>
-      <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
-          <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
+      <h1 className='h1-bold text-dark100_light900'>Saved Questions</h1>
 
-          <Link href='/ask-question' className='flex justify-end max-sm:w-full'>
-            <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900'>
-              Ask a Question
-            </Button>
-          </Link>
-      </div>
       <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
         <LocalSearchbar
           route='/'
@@ -33,13 +30,10 @@ const Home = async () => {
         />
 
         <Filter
-          filters={HomePageFilters}
+          filters={QuestionFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses="hidden max-md:flex"
         />
-
       </div>
-      <HomeFilter />
 
       <div className='mt-10 flex w-full flex-col gap-6'>
         {/* looping through questions */}
@@ -58,7 +52,7 @@ const Home = async () => {
              />
          ))
          : <NoResult
-            title='There’s no question to show'
+            title='There’s no saved question to show'
             description='Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. your query could be the next big thing others learn from. Get involved! 💡'
             link="/ask-question"
             linkTitle="Ask a Question"
@@ -70,4 +64,4 @@ const Home = async () => {
   )
 }
 
-export default Home
+export default Collection
