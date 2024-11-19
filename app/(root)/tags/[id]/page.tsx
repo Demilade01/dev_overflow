7a7fadfe2c1330bd7b8effdca0d576e65f941f93
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-11-19 06:04:31 -0800

    implement tag details page

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,44 +1,36 @@
-import QuestionCard from '@/components/cards/QuestionCard'
-import Filter from '@/components/shared/Filter'
-import NoResult from '@/components/shared/NoResult'
-import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
-import { QuestionFilters } from '@/constants/filter'
-import { getSavedQuestions } from '@/lib/actions/user.action'
-import { auth } from '@clerk/nextjs/server'
+import QuestionCard from '@/components/cards/QuestionCard';
+import NoResult from '@/components/shared/NoResult';
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import { IQuestion } from '@/database/question.model';
+import { getQuestionsByTagId } from '@/lib/actions/tag.actions';
+import { URLProps } from '@/types';
 import React from 'react'
 
-const Page = async () => {
-  const { userId } = await auth();
-
-  if(!userId) return null;
-
-  const result = await getSavedQuestions({
-    clerkId: userId,
+const Page = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q
   });
 
   return (
     <>
-      <h1 className='h1-bold text-dark100_light900'>Saved Questions</h1>
+      <h1 className='h1-bold text-dark100_light900'>{result.tagTitle}</h1>
 
-      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+      <div className='mt-11 w-full'>
         <LocalSearchbar
           route='/'
           iconPosition='left'
           imgSrc='/assets/icons/search.svg'
-          placeholder='Search saved questions'
+          placeholder='Search tag questions'
           otherClasses='flex-1'
-        />
-
-        <Filter
-          filters={QuestionFilters}
-          otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
       </div>
 
       <div className='mt-10 flex w-full flex-col gap-6'>
         {/* looping through questions */}
         { result.questions.length > 0 ?
-          result.questions.map((question: any) => (
+          result.questions.map((question: IQuestion) => (
              <QuestionCard
               key={question._id}
               _id={question._id}
@@ -60,7 +52,6 @@ const Page = async () => {
         }
       </div>
     </>
-
   )
 }
 
