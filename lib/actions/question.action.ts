886a55ddf0870a60311dbf09416e1b7c645d9427
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-12-06 02:02:04 -0800

    implement reputation in the codebase

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -93,10 +93,19 @@ export async function createQuestion(params: CreateQuestionParams) {
     await Question.findByIdAndUpdate(question._id, { $push: {tags: { $each: tagDocuments}} });
 
     // Create an interaction for the user ask_question action
+    await Interaction.create({
+      user: author,
+      action: "ask_question",
+      question: question._id,
+      tags: tagDocuments,
+    })
 
     // Increment AUTHOR'S REPUTATION by +5 points for creating a question
+    await User.findByIdAndUpdate(author, { $inc: { reputation: 5 }})
+
     revalidatePath(path);
   } catch (error) {
+    console.log(error);
 
   }
 }
@@ -143,7 +152,15 @@ export async function upvoteQuestion(params: QuestionVoteParams) {
       throw new Error('Question not found');
     }
 
-    // Increment author's reputation
+    // Increment author's reputation by +1/-1 by upvoting/revoking an upvote to the question
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasupVoted   ? -1 : 1}
+    })
+
+    // Increment author's reputation by +10/-10 for recieving an upvote/downvote  to the question
+    await User.findByIdAndUpdate(question.author, {
+      $inc: { reputation: hasupVoted ? -10 : 10 }
+    })
 
     revalidatePath(path);
 
@@ -179,6 +196,14 @@ export async function downvoteQuestion(params: QuestionVoteParams) {
     }
 
     // Increment author's reputation
+    await User.findByIdAndUpdate(userId,
+      { $inc : { reputation:  hasdownVoted ? -2 : 2 }
+    })
+
+    await User.findByIdAndUpdate(question.author,
+      { $inc : { reputation:  hasdownVoted ? -10 : 10 }
+    })
+
 
     revalidatePath(path);
 
